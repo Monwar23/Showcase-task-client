47bf19b2cf394d9c53c08f36778db1df242961d2
monwar23 <monwar2345@gmail.com>
2024-08-16 14:55:37 +0600

    create FirebaseProvider and complete provider functionality

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes'
+import FirebaseProvider from './firebase/FirebaseProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-       <RouterProvider router={router} />
+      <FirebaseProvider>
+      <RouterProvider router={router} />
+      </FirebaseProvider>
   </StrictMode>,
 )
